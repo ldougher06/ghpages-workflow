d939e873b5efe830c31e6d385dc4e5c7aaeb1fb6
logan dougher <logan.dougher@gmail.com>
2015-05-13 13:54:52 -0500

    copy other files during build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 #clean and prepare publib directory
 rm -rf public
-mkdir public
+cp -r src public
 
 #compile jade to html
 ./node_modules/.bin/jade src -P
@@ -22,3 +22,11 @@ rm -rf src/*.html \
 
 #convert ES6 JS to ES5
 ./node_modules/.bin/babel src --out-dir public -s inline
+
+#clean unneeded files
+rm -rf public/_styles \
+       public/*.jade \
+       public/**/*.jade \
+       public/*.scss \
+       public/**/*.scss
+
