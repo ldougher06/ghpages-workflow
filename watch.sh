cef6dc30c4f262c9516caf321a8f6e7bdccafe05
logan dougher <logan.dougher@gmail.com>
2015-05-14 15:39:55 -0500

    push before 21

--- watch.sh
+++ watch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#install dependencies
+
 npm install
 bower install
 
@@ -8,23 +8,30 @@ rm -rf public
 cp -r src public
 
 #compile jade to html
-./node_modules/.bin/jade src -0 public -PH
-rm -rf public/_partials
+./node_modules/.bin/nodemon -e jade --watch src --exec "
+  ./node_modules/.bin/jade src -0 public -PH
+  rm -rf src/*.html public/_partials" &
 
 #compile sass to css
 ./node_modules/.bin/node-sass \
   --output-style compressed \
   --source-map-embed \
-  src/_styles/main.scss public/css/main.css
+  --recursive \
+  src/_styles/main.scss public/css/main.css &
 
 #convert ES6 JS to ES5
-./node_modules/.bin/babel src --out-dir public -s inline
+./node_modules/.bin/babel \
+  src \
+  --out-dir public \
+  -s inline \
+  -w &
 
 #concat bower_components to public/lib directory
 if [ -d "bower_components" ]; then
 ./node_modules/.bin/bowcat . -o public/lib -m
 fi
 
+echo "clean"
 #clean unneeded files
 rm -rf public/_styles \
        public/*.jade \
@@ -32,3 +39,4 @@ rm -rf public/_styles \
        public/*.scss \
        public/**/*.scss
 
+echo "Watching for changes..."
